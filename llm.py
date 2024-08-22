3644f64d6dccee097b4f050d5014a20112be94ad
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-08-22 15:36:22 +0100

    Add dependencies installation in ci workflow

--- llm.py
+++ llm.py
@@ -1,16 +1,23 @@
 import os
+import sys
+
+from dotenv import load_dotenv
 
 from groq import Groq, GroqError, APIConnectionError
 from log import logger
 
 ConnectionError = APIConnectionError
 
+load_dotenv()
+
 
 def initiate_client():
     try:
-        client = Groq(api_key=os.environ.get("GROQ_KEY"))
+        client = Groq(api_key=os.getenv("GROQ_API_KEY"))
     except GroqError:
         logger.warning("Ensure the API key is set and valid")
+        sys.exit(1)
+
     return client
 
 
