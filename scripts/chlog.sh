3cef541063c20fcd718f13dab6125564ca0e5037
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-11-23 22:18:12 +0100

    revert: restore setuptools installation step in workflow
    This reverts commit a8cc4d5760dc46baaede71d662daa524a5d6ad4a.

--- scripts/chlog.sh
+++ scripts/chlog.sh
@@ -84,10 +84,8 @@ done < <(git log --pretty=format:"%s (%ad)" --date=short --reverse)
   echo "" # Ensure there's a newline at the end.
 } >> "$CHANGELOG_FILE"
 
-# Set the version in the VERSION file and update __init__.py and setup.py
-export version="$CURRENT_VERSION"
 echo "$version" > "VERSION"
 echo "VERSION = \"$version\"" > "litelookup/__init__.py"
 awk -v ver="$version" 'NR==3 {$0="version = \"" ver "\""} 1' setup.py > temp.py && mv temp.py setup.py
 
-echo "CHANGELOG.md and version files updated."
+echo "CHANGELOG.md updated."
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "0.19.6"
+version = "0.19.4"
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
