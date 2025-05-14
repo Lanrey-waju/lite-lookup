5b3956d716b792edce7fff8c5bcad8e9d777781d
Abdulmumin Akinde <lanreywaju97@gmail.com>
2025-05-14 16:22:24 +0100

    chore: update dependencies
    Update dependencies in `uv.lock` to their latest compatible versions.
    This includes updates for `langchain`, `langchain-core`, `langchain-groq`,
    `langchain-text-splitters`, `packaging` and `redis`.
    
    Also removes `dataclasses-json`, `marshmallow`, `mypy-extensions`,
    and `typing-inspect` dependencies.

--- litelookup/main.py
+++ litelookup/main.py
@@ -31,7 +31,7 @@ if os.environ.get("PYTHON_DEBUG", 0) == "1":
     debugpy.wait_for_client()
 
 logger = logging.getLogger(__name__)
-VERSION = "0.21.5"
+VERSION = "0.21.6"
 
 
 class InvalidInputError(Exception):
